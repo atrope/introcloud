38200e9063c8d0094e58202e5267d6da100b28b0
Alan Trope <atrope@gmail.com>
2017-12-06 13:46:24 +0200

    updates general

--- exercises/ex2/js/layout3.js
+++ exercises/ex2/js/layout3.js
@@ -1,18 +1,18 @@
-/* Random Hex Generator: paulirish.com/2009/random-hex-color-code-snippets
-*/
+/* Random Hex Generator: paulirish.com/2009/random-hex-color-code-snippets */
+  let MYUSERID = 24;
   function getRandomColor(){
     return "#" + (Math.random().toString(16) + "000000").slice(2, 8).toUpperCase().slice(-6);
   }
   function createSquare(){
-     return $('<article>', {
-       class: 'square',
+     return $('<rectangle>', {
        css: { "background-color": getRandomColor() }
     });
   }
   $(document).ready(function() {
-    for (var i = 0; i < 12; i++) $("main").append(createSquare());
+    let numRects = MYUSERID/2;
+    for (var i = 0; i < numRects; i++) $("main").append(createSquare());
 
-  $("main").on("click",".square",function(){
+  $("main").on("click","rectangle",function(){
     if ($(this).is(':last-child')) $("main").prepend(createSquare());
     else $(this).toggleClass("starred");
   })
