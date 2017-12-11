4e36f6dfa3e781679f9ef66f356bdaec56661c61
Alan Trope <atrope@gmail.com>
2017-12-11 12:24:38 +0200

    One CSSto rule them all

--- exercises/ex2/js/layout3.js
+++ exercises/ex2/js/layout3.js
@@ -4,17 +4,13 @@
     return "#" + (Math.random().toString(16) + "000000").slice(2, 8).toUpperCase().slice(-6);
   }
   function createSquare(){
-     return $('<rectangle>', {
-       css: { "background-color": getRandomColor() }
-    });
+     return $('<rectangle>', {class:"live",css: { "background-color": getRandomColor() }});
   }
   $(document).ready(function() {
     let numRects = MYUSERID/2;
     for (var i = 0; i < numRects; i++) $("main").append(createSquare());
-
-  $("main").on("click","rectangle",function(){
-    if ($(this).is(':last-child')) $("main").prepend(createSquare());
-    else $(this).toggleClass("starred");
-  })
-
+    $("main").on("click","rectangle",function(){
+      if ($(this).is(':last-child')) $("main").append(createSquare());
+      else $(this).toggleClass("starred");
+    })
   });
