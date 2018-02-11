0d2f724c597ff09d2c9aee08e6097e66590e0e38
Alan Trope <atrope@gmail.com>
2018-02-12 00:30:39 +0200

    forgot bookmark

--- findMyClothes/js/main.js
+++ findMyClothes/js/main.js
@@ -137,7 +137,10 @@ $(document).ready(function() {
 
         }
     });
-
+    $('body').on('click','.recommendations .bookmark,.product .bookmark',function(e){
+      e.preventDefault();
+      $(this).find(".fa").toggleClass("fa-bookmark-o").toggleClass("fa-bookmark");
+    });
     $('body').on('click', '.map', function(e) {
         swal({
             html: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.1410855591716!2d34.77445461516411!3d32.065432581192695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4b7df683586b%3A0xc95a69f356f7f3b2!2sSderot+Rothschild%2C+Tel+Aviv-Yafo!5e0!3m2!1spt-BR!2sil!4v1515023771237" width="1" height="200" frameborder="0" style="border:0;width:100%;" allowfullscreen></iframe>'
